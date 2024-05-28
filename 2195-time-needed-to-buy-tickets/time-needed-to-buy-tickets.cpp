60f24b8bc18c7d20955e40a94de83bc01cfb8710
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-05-28 11:34:59 +0800

    Time: 0 ms (100.00%) | Memory: 9.3 MB (53.77%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<int> people; 
+        int numTickets = tickets[k]; // minimally number of rounds; 
+        int time = 0; 
         for (int i = 0; i < tickets.size(); ++i) {
-            if (tickets[i] > 0) people.push(i); 
-        }
-        int time = 0;
-        while (!people.empty()) {
-            int curIdx = people.front(); 
-            people.pop();
-            tickets[curIdx] -= 1;
-            time++; 
-            if (curIdx == k && tickets[curIdx] == 0) {
-                return time; 
+            if (i <= k) {
+                time += min(numTickets, tickets[i]); 
             }
-            if (tickets[curIdx] > 0) people.push(curIdx);
+            else {
+                time += min(numTickets - 1, tickets[i]); 
+            } 
         }
-        return -1; 
+        return time; 
     }
 };
+/*
+if person infront me has >= tickets as me, then i need to wait
+the same number of rounds as me
+but if the person infront of me < tickets then me, then i only
+need to wait his number of rounds (while he is in the queue) 
+
+what about the person behind me? 
+if they have more tickets than me, then i only need to wait numTickets -1
+round since on the last round, i'll be infornt of them and it'll be over
+if they have less tickets than me, then it just their tickets
+*/
