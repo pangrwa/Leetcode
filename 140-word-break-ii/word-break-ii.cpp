928a693998f7a5a574cbd96499feac2b810535fb
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-05-29 21:43:41 +0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (68.30%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -2,55 +2,46 @@ class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict; 
-        for (const auto& s : wordDict) {
+        for (string s : wordDict) {
             dict.insert(s); 
         }
-
         vector<vector<string>> dp(s.size() + 1, vector<string>(0, ""));
         dp[0].push_back(""); 
-        
-        for (int i = 0; i < s.size(); ++i) {
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
             for (int j = 0; j <= i; ++j) {
-                string cur = s.substr(j, i - j + 1); 
-                if (dict.contains(cur)) {
-                    for (const auto& s : dp[j]) {
-                        string stn = s; // make a copy
-                        if (s != "") {
-                            stn += " "; 
-                        }
-                        stn += cur; 
-                        dp[i + 1].push_back(stn); 
+                string curWord = s.substr(j, i - j + 1); 
+                if (!dict.contains(curWord)) {
+                    continue; 
+                }
+                for (string stn : dp[j]) {
+                    string copy = stn; 
+                    if (copy != "") {
+                        copy += " ";
                     }
+                    copy += curWord; 
+                    dp[i + 1].push_back(copy); 
                 }
             }
         }
-        return dp[s.size()]; 
+        return dp[n]; 
     }
 };
 
 /*
-how do we break the problem down? 
-catsand         dog 
-(subproblem)    (if this is in dict, add to subproblem) 
-the subproblem must also contains a valid sentence
-example: catsan ddo if ddo is a valid word but catsan didnt form a 
-    valid sentence, this would be invalid
 
-f(i) = all(f(j) + s[j..i]) where j: 0 -> i and s[j..i] in dict and f(j) 
-    contains valid sentences 
-base case: f(0) = ""
-and valid.insert(0)
 
-how to to know if f(j) contains valid sentences? 
-we keep track of the indices where it's possible to break into valid 
-    sentences
 
-iterate across the string
-for each char i, represents the end of a possible word we're forming
-    iterate from start to that char, j
-        if the word s[j..i] is in the dicitonary
-            for  all the sentences in the subproble dp[j] (valid.contains(j))
-                dp[i].append(setence + " "  + s[j..i])
-return dp[n] 
+iterate across each char in s
+    iterate from 0 -> s
+        if there's a valid string
+            dp[i] = for all valid sentences in dp[j - 1] add this word
+// the reason why we can assume that we will have a full sentence 
+    is because if we try to start from somewhere in the middle instead
+    of the start for example d[j], the vector length is empty
+        hence there is no valid sentence for you to extend
+    so all sentences have to start from the beginnign. 
+
+catsanddog, "sand, dog"
 
 */ 
