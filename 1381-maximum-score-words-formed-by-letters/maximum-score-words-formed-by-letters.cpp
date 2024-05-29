3a8dd6fd57d07e4861735577840c20bc8bfdff3f
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-05-29 11:01:56 +0800

    Time: 3 ms (82.90%) | Memory: 13.9 MB (47.19%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -1,60 +1,62 @@
 class Solution {
 public:
-    unordered_map<char, int> letterCount;
-    unordered_map<char, int> wordCounter; 
-    int res = numeric_limits<int>::min(); 
-    int scoreTracer = 0; 
+    unordered_map<char, int> lettersUsed; 
+    unordered_map<char, int> lettersHave;
+    int scoreTracker = 0; 
+    int res = 0; 
 
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        for (int i = 0; i < letters.size(); ++i) {
-            letterCount[letters[i]]++; 
+        for (char c : letters) {
+            lettersHave[c]++; 
         }
-
-        backtrack(0, words, score);
+        backtrack(0, words, score); 
         return res; 
     }
 
-    void backtrack(int idx, vector<string>& words, vector<int>& score) {
+    void backtrack(int idx, vector<string>& words, vector<int>& score) { 
         if (idx == words.size()) {
-            res = max(res, scoreTracer); 
+            res = max(res, scoreTracker); 
             return; 
         }
-        // don't take current word
+
+        // don't use the current word
         backtrack(idx + 1, words, score); 
 
-        // check if it's possible to take current word
-        for (char c : words[idx]) {
-            wordCounter[c]++; 
-            scoreTracer += score[c - 'a'];
+        // use the current word
+        string cur = words[idx]; 
+        for (char c : cur) { 
+            lettersUsed[c]++;
+            lettersHave[c]--; 
+            scoreTracker += score[c - 'a']; 
         }
-        
-        if (validToRemove()) {
+        // check if valid to use current word
+        bool ready = true; 
+        for (char c : cur)  {
+            if (lettersHave[c] < 0) { // invalid use
+                ready = false; 
+            }
+        }
+
+        if (ready) {
             backtrack(idx + 1, words, score); 
         }
 
-        // rollback
-        for (char c : words[idx]) {
-            wordCounter[c]--; 
-            scoreTracer -= score[c - 'a']; 
+        // remove it
+        for (char c : cur) {
+            lettersUsed[c]--;
+            lettersHave[c]++; 
+            scoreTracker -= score[c - 'a']; 
         }
-    }
-
-    bool validToRemove() {
-        for (const auto& pair : wordCounter) {
-            if (letterCount[pair.first] < pair.second) {
-                return false;
-            }
-        }
-        return true; 
     }
 };
 
 /*
-
-either take the word or dont take the word
-- if possible, take the word and remove corresopnding letters
-- else don't take the word 
-basicaly form all the possible subsets of the words if possible
-how do we determine if we have enough letters to give out? count
-the letters we have left 
+basically like forming subsets of the words
+- either choose the current word or not
+- how to know if we can choose the current word or not? 
+    - keep track of the remaining letters, and check if the freq letters
+        matches with the word
+    
+If we use DP, we have to track the remaining letters from the previous
+subproblem 
 */ 
