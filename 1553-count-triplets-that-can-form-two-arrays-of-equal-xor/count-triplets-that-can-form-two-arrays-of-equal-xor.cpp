087b8f398446224e142e893ea129b8a7ef0fc827
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-05-31 11:42:13 +0800

    Time: 0 ms (100.00%) | Memory: 9.1 MB (50.11%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int n = arr.size(); 
+        vector<int> prefixSum;
+        prefixSum.push_back(0); // start point
+        int a = 0; 
+        for (int i : arr) {
+            a ^= i; 
+            prefixSum.push_back(a); 
+        }
+
         int res = 0; 
-        int prefixSum = 0; 
-        for (int i = 0; i < n; ++i) {
-            prefixSum = arr[i]; 
-            for (int j = i + 1; j < n; ++j) {
-                prefixSum ^= arr[j];
-                if (prefixSum == 0)
-                    res += j - i; 
+        for (int i = 0; i < prefixSum.size(); ++i) {
+            for (int j = i + 1; j < prefixSum.size(); ++j) {
+                if (prefixSum[i] == prefixSum[j]) {
+                    res += (j - (i + 1)); 
+                }
             }
         }
         return res; 
