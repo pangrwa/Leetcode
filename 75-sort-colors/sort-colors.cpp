588e916a10ee95e74c4c844f2a277407a3bdb391
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-06-12 11:51:28 +0800

    Time: 0 ms (100.00%) | Memory: 9.9 MB (34.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> count(3, 0);
-        for (int num : nums) {
-            count[num]++; 
-        }
-        nums.clear();
-        for (int i = 0; i < count.size(); ++i) {
-            while (count[i] > 0) {
-                nums.push_back(i);
-                count[i]--; 
+        
+        int left = 0, right = nums.size() - 1; 
+        for (int i = 0; i < nums.size(); ++i) {
+            while (i >= left && i <= right && (
+                (nums[i] == 0 || nums[i] == 2)
+            )) {
+                if (nums[i] == 0) {
+                    swap(nums[left++], nums[i]); 
+                } else {
+                    swap(nums[right--], nums[i]); 
+                }
             }
         }
     }
@@ -18,5 +20,10 @@ public:
 /*
 since it's a in-place algorithm we can write our own sorting algorithm in this case
     we could use merge sort but that will take up n space for combin
-    we could use counting sort 
+    we could use counting sort in O(1) space but that is not in 1 pass
+How we could do it in 1 pass
+    - identify 3 elements -> move 2 the back and 0 to the front
+        the issue with appending element to the front is it take 
+        O(n) space
+    - use two pointers to guide where to swap 0s and 2s with
 */ 
