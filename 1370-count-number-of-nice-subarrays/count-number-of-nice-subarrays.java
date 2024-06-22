f6857a5d4d68d5bd1090d0ef2fd49c6cf734d549
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-06-22 12:56:27 +0800

    Time: 2532 ms (5.05%) | Memory: 55.5 MB (12.84%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int oddCounter = 0;
+        int n = nums.length; 
+        int left = 0;
+        int right = 0; 
+        int res = 0; 
+        
+        while (right < n) {
+            int cur = nums[right];
+            if (cur % 2 != 0) {
+                oddCounter++; 
+            }
+            if (oddCounter == k) {
+                // count the number of even numbers on the right
+                int ext = right + 1; 
+                while (ext < n && (nums[ext] % 2 == 0)) {
+                    ext++; 
+                }
+                int numEven = ext - right; 
+                
+                while (oddCounter == k) {
+                    System.out.printf("Left: %s, right: %s\n", left, right); 
+                    if (nums[left] % 2 != 0) {
+                        oddCounter--; 
+                    }
+                    res += numEven; 
+                    left++;
+                }
+            }
+            right++; 
+        }
+        return res; 
+    }
+}
+
+/*
+counting the total number of odd numbers is important, because 
+*/ 
