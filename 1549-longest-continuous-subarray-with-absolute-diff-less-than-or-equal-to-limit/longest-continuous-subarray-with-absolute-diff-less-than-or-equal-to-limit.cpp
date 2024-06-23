52c23406fd6ba474c90a2fa60a75ab16b28e3f80
Rui Wei <76247607+pangrwa@users.noreply.github.com>
2024-06-23 13:21:31 +0800

    Time: 61 ms (85.15%) | Memory: 52.1 MB (94.01%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void print(deque<int> temp) {
+        while (!temp.empty()) {
+            cout << temp.front() << " ";
+            temp.pop_front(); 
+        }
+        cout << endl; 
+    }
+    int longestSubarray(vector<int>& nums, int limit) {
+        int left = 0;
+        int right = 0;
+        int n = nums.size(); 
+        int res = 0;
+        deque<int> maxElements;
+        deque<int> minElements;
+        while (right < n) {
+            int cur = nums[right];
+            while (!maxElements.empty() && cur >= nums[maxElements.back()]) {
+                maxElements.pop_back();
+            }
+            maxElements.push_back(right);
+            while (!minElements.empty() && cur <= nums[minElements.back()]) {
+                minElements.pop_back();
+            }
+            minElements.push_back(right);
+            // cout << "Left: " << left << " right: " << right << " max: " << maxElements.front() << " min: " << minElements.front() << '\n';
+
+            // print(maxElements);
+            // print(minElements); 
+            while (left <= right && abs(nums[maxElements.front()] - nums[minElements.front()]) > limit) {
+                left++; 
+                // clear the of bounds stack idx
+                while (maxElements.front() < left) {
+                    maxElements.pop_front();
+                }
+                while (minElements.front() < left) {
+                    minElements.pop_front(); 
+                }
+            }
+            // cout << "divier" << endl; 
+            // print(maxElements);
+            // print(minElements); 
+            // cout << "Left: " << left << " right: " << right << " max: " << maxElements.front() << " min: " << minElements.front() << '\n';
+
+            res = max(res, right - left + 1);
+            right++;
+        }
+        return res;
+    }
+};
